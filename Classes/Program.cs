30974584838079a2ad19411ffa831d9457c10e6c
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2023-10-25 23:31:24 +0300

    Class ve Encapsulation

---
+++ Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Rabia";
+            customer.LastName = "Kandemir";
+            customer.City = "Ankara";
+            Customer customer2 = new Customer()
+            {
+                Id = 2,
+                FirstName="Engin",
+                LastName="Demiroğ",
+                City="İstanbul"
+            };
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+}
